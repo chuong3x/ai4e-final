ceb97065361c388284e8c845d2d9059fba36cffc
ChuongDo <chuongdotu@gmail.com>
2023-11-26 00:11:55 +0700

    change model type to .h5, change solution file

--- services/ml/index.py
+++ services/ml/index.py
@@ -1,20 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from tensorflow import keras
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)  # Cấu hình CORS cho tất cả các route
 # Load model vào bộ nhớ khi server khởi động
+# Đọc lại mô hình đã huấn luyện
+model = keras.models.load_model('./my_model.h5')
 ###
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()  # Nhận dữ liệu từ client
-        # Thực hiện dự đoán với model và dữ liệu nhận được từ client
         print(data)
+        # Chuyển đổi dữ liệu thành định dạng mà mô hình yêu cầu
+        features = np.array(data['features']).reshape(1, -1)
+
+        # Dự đoán kết quả
+        # prediction = model.predict(features)
+        # print(prediction)
 
         # Trả về kết quả dự đoán dưới dạng JSON
-        return jsonify({'prediction': "a"})
+        return jsonify({'prediction':" prediction[0].tolist()"})
     except Exception as e:
         return jsonify({'error': str(e)})
 
